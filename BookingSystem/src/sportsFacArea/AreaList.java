49bade262a16e58d4e332e7bdb1496c8ecbf2efe
oslob99 <jkh991116@naver.com>
2023-04-11 18:39:48 +0900

    # 4/11 지역 스포츠 정하기 생성

--- BookingSystem/src/sportsFacArea/AreaList.java
+++ BookingSystem/src/sportsFacArea/AreaList.java
@@ -3,17 +3,6 @@ package sportsFacArea;
 import java.util.List;
 
 public class AreaList {
-    static List<String> area;
 
-    static {
-        SportsFacList sportsFacilityList = new SportsFacList("축구장","농구장","수영장");
 
-//        area=new HashMap<>();
-
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println(area.toString());
-    }
 }
---
+++ BookingSystem/src/sportsFacArea/Test.java
@@ -0,0 +1,9 @@
+package sportsFacArea;
+
+public class Test {
+
+    public static void main(String[] args) {
+        BookingView bv = new BookingView();
+        bv.areaStart();
+    }
+}
