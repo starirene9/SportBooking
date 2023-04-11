afebb7b04e93e528e0b2755245a02333a717a722
lsulsulsu <126743189+TTTWIXX@users.noreply.github.com>
2023-04-11 19:25:22 +0900

    0411 19:25

--- BookingSystem/src/userSys/UserView.java
+++ BookingSystem/src/userSys/UserView.java
@@ -1,5 +1,7 @@
 package userSys;
 
+import java.util.Scanner;
+
 import static userSys.Utility.*;
 
 public class UserView {
@@ -7,23 +9,25 @@ public class UserView {
     UserInfo userInfo;
     private static UserRepository urp;
 
+
     static {
         uv = new UserView();
         urp = new UserRepository();
     }
 
-    public void joinMemberShip() {
+    private void joinMemberShip() {
         System.out.println("==========[ 회 원 가 입 ]==========");
 
         // 아이디 입력
         String userId;
         while (true) {
             userId = input("# 아이디 : ");
+
             // 아이디 중복검사
             if (urp.isDuplicate(userId)) {
                 System.out.println("아이디 중복입니다. 다시 입력해주세요");
             } else {
-                if (userId!="") {
+                if (!userId.equals("")) {
                     System.out.println("입력 가능한 아이디 입니다.");
                     break;
                 } else {
@@ -64,11 +68,16 @@ public class UserView {
         //생년월일 입력
         String userAge;
         while (true) {
-            userAge = input("생년월일(6자리) : ");
+            userAge = input("생년월일(8자리) : ");
             // 올바른 값 확인 && 글자수 제한
-            if (isIntNum(userAge) && stringLength(userAge, 6)) {
+            if (isIntNum(userAge) && stringLength(userAge, 8)) {
                 break;
-            } else {
+            }
+        }
+        while(true){
+            if(checkAge(userAge)){
+                System.out.println("굿");
+                break;
             }
         }
 
@@ -82,13 +91,13 @@ public class UserView {
     }
 
     /**
-     * String을 int로 치환하는 함수
+     * String 을 int 로 치환하는 함수
+     *
      * @return true 모두 숫자이다.
      * flase 숫자로 바꿀수 없는 값이 있다.
      * @param1 확인하고싶은 String값
      */
-
-    private boolean isIntNum(String userAge) {
+    boolean isIntNum(String userAge) {
 
         try {
             int num = Integer.parseInt(userAge);
@@ -103,6 +112,7 @@ public class UserView {
 
     /**
      * 글자수를 제한하는 함수
+     *
      * @return 글자수가 제한글자수를 초과할시 false;
      * 글자수가 제한글자수보다 작을시 true;
      * @param1 : String 타입 변수
@@ -120,6 +130,58 @@ public class UserView {
 
         joinMemberShip();
         urp.showList();
+    }
+
+    /**
+     * 올바른 생년월일 인지 검사하는 기능
+     *
+     * @return true 알맞은 생년월일
+     * false 부정확한 생년월일
+     * @param1 정수로 변환될수있는 String
+     */
+
+    boolean checkAge(String age) {
+
+        int month = Integer.parseInt(age.substring(4, 6));
+        int day = Integer.parseInt(age.substring(6, 8));
+        if (month > 0 && month < 12) {
+            switch (month) {
+                case 1, 3, 5, 7, 8, 10, 12:
+                    if (day <= 31) {
+                        System.out.println("올바른 값");
+                        return true;
+                    }
+                case 4, 6, 9, 11:
+                    if (day <= 30) {
+                        System.out.println("올바른 값");
+                        return true;
+                    }
+                case 2:
+                    if (checkYear(age) == "윤년" && day <= 29) {
+                        System.out.println("올바른 값");
+                        return true;
+                    } else if (checkYear(age) == "평년" && day <= 28) {
+                        System.out.println("올바른 값");
+                        return true;
+                    } else {
+                        return false;
+                    }
+                default:
+                    return false;
+
+            }
+        }
+        return false;
+    }
+
+    String checkYear(String age) {
+        int year = Integer.parseInt(age.substring(0, 4));
+        if (year % 4 == 0 && (year % 4) != 0) {
+            return "윤년";
+        } else if (year % 4 == 0) {
+            return "윤년";
+        } else
+            return "평년";
     }
 
 
